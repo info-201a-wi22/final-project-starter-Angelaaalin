cb03831f234bcb539d31376df1f584be8d410453
liuguri <rliu3@uw.edu>
2022-02-18 15:55:53 -0800

    choropleth map of average temperature in the U.S

--- source/U.S_Temperature_Overview.R
+++ source/U.S_Temperature_Overview.R
@@ -1,4 +1,4 @@
-#load packages
+# Load packages
 install.packages("ggplot2")
 install.packages("dplyr")
 install.packages("mapproj")
@@ -6,21 +6,21 @@ library("ggplot2")
 library("dplyr")
 library("mapproj")
 
-#read data
+# Read data
 temp_by_state <- read.csv(file = "https://media.githubusercontent.com/media/info-201a-wi22/final-project-starter-Angelaaalin/main/data/gloabl_temp_archive/GlobalLandTemperaturesByState.csv", header = TRUE, stringsAsFactors = FALSE)
 View(temp_by_state)
 
-#select the latest data of average temperature in the U.S
+# Select the latest data of average temperature in the U.S
 new_data <- temp_by_state %>%
   filter(dt == "2013-08-01") %>%
   mutate(State = tolower(State))
 
-#join temperature data to the U.S shapefile
+# Join temperature data to the U.S shapefile
 state_shape <- map_data("state") %>%
   rename(State = region) %>%
   left_join(new_data, by = "State")
   
-#draw the map setting the 'fill' of the each state using its average temperature
+# Draw the map setting the 'fill' of the each state using its average temperature
 ggplot(state_shape) +
   geom_polygon(
     mapping = aes(x = long, y = lat, group = group, fill = AverageTemperature),
