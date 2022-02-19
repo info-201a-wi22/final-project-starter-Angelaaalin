65506aee158450f9c4ecba9c1d94d2ecc5f4f4c9
Angela <alin28@uw.edu>
2022-02-18 16:37:48 -0800

    update

--- source/Summary Information Script.R
+++ source/Summary Information Script.R
@@ -13,7 +13,7 @@ temp_by_city <- read.csv(file = "https://media.githubusercontent.com/media/info-
 c_emission <- read.csv(file = "https://media.githubusercontent.com/media/info-201a-wi22/final-project-starter-Angelaaalin/main/data/annual-co-emissions-by-region.csv", header = TRUE, stringsAsFactors = FALSE)
 View(temp_by_state)
 
-#Change of global average land temperature from 18th century to 21th century#
+#Change of global average land temperature from 18th century to 19th century#
 temp_18 <- global_temp %>% 
   filter(dt >="1700-01-01",dt <= "1799-12-01") %>%
   pull(LandAverageTemperature)
@@ -24,33 +24,30 @@ temp_19 <- global_temp %>%
   pull(LandAverageTemperature)
 ave_land_temp_19 <- mean(temp_19, na.rm = TRUE) 
 
+temp_18_19 <- ave_land_temp_19 - ave_land_temp_18
+
+#Change of global average land temperature from 19th century to 20th century#
 temp_20 <- global_temp %>% 
   filter(dt >="1900-01-01",dt <= "1999-12-01") %>%
   pull(LandAverageTemperature)
 ave_land_temp_20 <- mean(temp_20, na.rm = TRUE) 
 
+temp_19_20 <- ave_land_temp_20 - ave_land_temp_19
+
+#Change of global average land temperature from 20th century to 21st century#
 temp_21 <- global_temp %>% 
   filter(dt >="2000-01-01",dt <= "2099-12-01") %>%
   pull(LandAverageTemperature)
 ave_land_temp_21<- mean(temp_21, na.rm = TRUE) 
-
-v1 <- c(ave_land_temp_18, ave_land_temp_19, ave_land_temp_20, ave_land_temp_21)
-v2 <- c(NA, ave_land_temp_18, ave_land_temp_19, ave_land_temp_20)
-change_of_land_temp <- v1 - v2
-change_century <- data.frame(Century = c("18", "19", "20", "21"),
-           "Change of land temperature" = change_of_land_temp )
+temp_20_21 <- ave_land_temp_21 - ave_land_temp_20
 
 #Average temperature of countries after 2000#
-average_temp_after_2000 <- temp_by_country %>%
+temp_after_2000 <- temp_by_country %>%
   filter(dt >= "2000-01-01") %>%
   group_by(Country) %>%
   summarise(ave_temp = mean(AverageTemperature, na.rm = TRUE))
+ave_temp_2000 <- mean(temp_after_2000$ave_temp, na.rm = TRUE)
 
-#Average annual CO2 emission of entities#
-average_CO2 <- c_emission %>%
-  group_by(Entity) %>%
-  summarise(ave_CO2_emission = mean(Annual.CO2.emissions..zero.filled., na.rm = TRUE))
-View(average_CO2)
 #Entity with highest average annual CO2 emission#
 max_CO2 <- average_CO2 %>%
   filter(Entity != "World" ) %>%
@@ -78,9 +75,10 @@ highest_temp_state <- temp_by_state %>%
 
 #Summary Information List#
 summary_info <- list(
-  "Global average temperature change of centuries" = change_century, 
-  "Average temperature of countiesaverage after 2000" = average_temp_after_2000,
-  "Average annual CO2 emission of entites" = average_CO2, 
+  "Global average temperature changes from 18th century to 19th century" = temp_18_19,
+  "Global average temperature changes from 19th century to 20th century" = temp_19_20,
+  "Global average temperature changes from 20th century to 21st century" = temp_20_21,
+  "Average temperature of counties after 2000" = ave_temp_2000,
   "Entity with highest CO2 emission" = max_CO2, 
   "City with highest average temperature in the mosr recent year" = highest_temp_city, 
   "State with highest average temperature in 21th century" = highest_temp_state)
