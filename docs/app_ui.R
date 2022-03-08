8844c22d93cc63a09c8085e46f6584b7bd197996
Angela <alin28@uw.edu>
2022-03-07 19:11:35 -0800

    update

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -5,13 +5,9 @@ library("shiny")
 
 source("../tabs/interactive1.R")
 source("../tabs/interactive2.R")
-<<<<<<< HEAD
 source("../tabs/interactive3.R")
 source("../tabs/introduction.R")
-=======
-source("../tabs/introduction.R")
 
->>>>>>> 9511f4f43864834bb16bde0067d209792424f8b0
 #App UI
 ui <- navbarPage("Climate Change",
                  intro_page,
